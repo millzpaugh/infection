108956002ab3c04f6babbcc56bf3dc1a79b5fec8
Ann Millspaugh <ann.millspaugh@gmail.com>
2016-02-02 16:49:26 -0800

    gitignore again

--- project/settings/live.py
+++ project/settings/live.py
@@ -6,4 +6,4 @@ import dj_database_url
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-DATABASES =  {'default':'postgres://wqbahqraoftxuf:_NkBP1KD9_df7Q-t0Qq2XRpAAc@ec2-54-225-199-245.compute-1.amazonaws.com:5432/d1q238v1m1l86l'}
+DATABASES =  {'default': dj_database_url.config()}
